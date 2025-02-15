b496ed4dd64764a8566d33262bcdb2a1493c5c5a
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-16 00:49:37 +0530

    added assests and fonts and colors to project , also started creating sign-in screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
-import React from "react";
+import { SplashScreen, Stack } from "expo-router";
+import React, { useEffect } from "react";
 import './globals.css'
+import { useFonts } from "expo-font"
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsloaded] = useFonts({
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+  })
+
+  useEffect(() => {
+    if (fontsloaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsloaded])
+
+  if (!fontsloaded) return null
+
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import { Image, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '@/constants/images'
 
 const signin = () => {
     return (
-      <View>
-        <Text> Sign In </Text>
-      </View>
+      <SafeAreaView className='bg-white h-full'>
+        <ScrollView contentContainerClassName='h-full'>
+          <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain'/>
+        </ScrollView>
+      </SafeAreaView>
     )
 }
 
