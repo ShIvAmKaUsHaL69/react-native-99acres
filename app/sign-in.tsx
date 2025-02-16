6255a86edeccb53247543c593f4ada112d04d82d
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-16 22:31:15 +0530

    UI for signin screen and oauth setup using appwrite

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
-import { Image, ScrollView } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import images from '@/constants/images'
+import icons from '@/constants/icons'
 
 const signin = () => {
+  const handlelogin = () => {
+
+  }
     return (
       <SafeAreaView className='bg-white h-full'>
         <ScrollView contentContainerClassName='h-full'>
-          <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain'/>
+          <Image source={images.onboarding} className='w-full h-4/6 mt-5 contain-size' resizeMode='contain'/>
+          <View className='px-10'>
+            <Text className='text-base text-center font-rubik uppercase text-black-200'>Welcome to Restate</Text>
+            <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'>
+              Let's Get You Closer to {"\n"}
+                <Text className='text-primary-300'>Your Ideal Home</Text>
+            </Text>
+            <Text className='text-lg font-rubik text-black-200 text-center mt-8'>Login to Restate with Google</Text>
+            <TouchableOpacity onPress={handlelogin} className='bg-white shadow-md shadow-zinc-400 rounded-full w-full py-4 mt-5'>
+              <View className='flex flex-row items-center justify-center'>
+              <Image source={icons.google} className='w-5 h-5' resizeMode='contain'/>
+              <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with Google</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </SafeAreaView>
     )
---
+++ lib/appwrite.ts
@@ -0,0 +1,69 @@
+import {Account, Avatars, Client, OAuthProvider} from 'react-native-appwrite';
+import * as Linking from 'expo-linking';
+import * as WebBrowser from 'expo-web-browser'
+
+export const config = {
+    Platform: 'com.shivam.restate',
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
+
+export const client = new Client();
+client.setEndpoint(config.endpoint!).setProject(config.projectId!).setPlatform(config.Platform!)
+
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+    try {
+        const redirecturi = Linking.createURL('/');
+        const responce = await account.createOAuth2Token(OAuthProvider.Google, redirecturi);
+        if(!responce) throw new Error('Failed to Login')
+
+        const browserresult = await WebBrowser.openAuthSessionAsync(responce.toString(), redirecturi)
+
+        if(browserresult.type !== 'success') throw new Error('Failed to Login')
+        
+        const url = new URL(browserresult.url);
+        const secreat = url.searchParams.get('secret')?.toString();
+        const userid = url.searchParams.get('userId')?.toString();
+
+        if(!secreat || !userid) throw new Error('Failed to Login')
+
+        const session = await account.createSession(userid, secreat)
+        if (!session) throw new Error('Failed to create session')
+
+        return true;
+
+    } catch (error) {
+        console.log(error)
+        return false;
+    }
+}
+
+export async function logout() {
+    try {
+        await account.deleteSession('current');
+        return true
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
+
+export async function getuser() {
+    try {
+        const responce = await account.get();
+        if(responce.$id) {
+            const useravatar = avatar.getInitials(responce.name);
+            return {
+                ...responce,
+                avatar: useravatar.toString();
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
