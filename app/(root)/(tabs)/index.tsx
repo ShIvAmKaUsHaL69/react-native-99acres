273c552c78410ca2f4ed999a459fad7c7cee55bb
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-02-18 22:30:02 +0530

    implemented header of app

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,23 @@
-import React from "react";import { Text, View } from "react-native";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import React from "react";import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl my-10 font-rubik">Welcome to Restate</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className='flex flex-row items-center justify-between mt-5'>
+          <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="size-12 rounded-full"/>
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                <Text className="text-base font-rubik-medium text-black-300">Shivam</Text>
+              </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
